d82d1b87ccc43008beec68789f8a4a7eb3a6cd58
Jntuhcse93 <ucejcse39@gmail.com>
2024-12-12 12:24:19 +0530

    Update addtwo.java

--- addtwo.java
+++ addtwo.java
@@ -1,16 +1,16 @@
-public class addtwo{
-public static void main (String[] args){
-	if(args.length<2){
-		System.out.println("Please provide two numbers as arguments");
-		return;
-	}
-	//parse the numbers fromcommand -line arguments
-	int num1= Interger.parseInt(args[0]);
-	int num2= Interger.parseInt(args[1]);
-	
-	int sum = num1+num2;
-	
-	System.out.println("the sum of " + num1 + " and " + num2 +" is :" +sum);
-	}
+public class addtwo {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as arguments");
+            return;
+        }
+        
+        // Parse the numbers from command-line arguments
+        int num1 = Integer.parseInt(args[0]);  // Corrected typo here
+        int num2 = Integer.parseInt(args[1]);  // Corrected typo here
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
 }
-	
